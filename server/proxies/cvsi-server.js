baae81f1fcb0a1faa2919224ecc7b6b6381e3642
Andrei Luca <thendrluca@gmail.com>
2016-07-08 10:50:03 +0300

    added proxy

---
+++ server/proxies/cvsi-server.js
@@ -0,0 +1,18 @@
+/*jshint node:true*/
+var proxyPath = '/cvsiServer';
+
+module.exports = function(app) {
+  // For options, see:
+  // https://github.com/nodejitsu/node-http-proxy
+  var proxy = require('http-proxy').createProxyServer({});
+
+  proxy.on('error', function(err, req) {
+    console.error(err, req.url);
+  });
+
+  app.use(proxyPath, function(req, res, next){
+    // include root path in proxied request
+    req.url = proxyPath + '/' + req.url;
+    proxy.web(req, res, { target: 'http://192.168.3.146:8080' });
+  });
+};
