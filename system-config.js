339069c20375d8d34e56490f7a70f7e6d1d0bb9e
Andrei Luca <thendrluca@gmail.com>
2016-07-18 16:09:12 +0300

    new gh-pages version

---
+++ system-config.js
@@ -0,0 +1,66 @@
+// SystemJS configuration file, see links for more information
+// https://github.com/systemjs/systemjs
+// https://github.com/systemjs/systemjs/blob/master/docs/config-api.md
+/***********************************************************************************************
+ * User Configuration.
+ **********************************************************************************************/
+/** Map relative paths to URLs. */
+var map = {
+    'material-design-lite': 'material-design-lite/dist/material.js'
+};
+/** User packages configuration. */
+var packages = {
+    'material-design-lite': {
+        format: 'cjs'
+    }
+};
+////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************************
+ * Everything underneath this line is managed by the CLI.
+ **********************************************************************************************/
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/forms',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    // Thirdparty barrels.
+    'rxjs',
+    // App specific barrels.
+    'app',
+    'app/shared',
+    'app/home',
+    'app/auth',
+    'app/register',
+    'app/forgot-password',
+    'app/login',
+    'app/products-list',
+    'app/my-items',
+    'app/product-details',
+    'app/logout',
+    'app/profile',
+    'app/settings',
+    'app/terms',
+    'app/about',
+    'app/new-product',
+];
+var cliSystemConfigPackages = {};
+barrels.forEach(function (barrelName) {
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+// Apply the CLI SystemJS configuration.
+System.config({
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js'
+    },
+    packages: cliSystemConfigPackages
+});
+// Apply the user's configuration.
+System.config({ map: map, packages: packages });
+//# sourceMappingURL=system-config.js.map
